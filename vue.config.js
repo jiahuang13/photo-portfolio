73f26e01b52cbbb20dda9039d7e1cb67fce577cd
jiahuang <icecream3512@gmail.com>
2024-02-22 22:11:21 +0800

    vue deploy files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
+const path = require("path");
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/photo-portfolio/" : "/",
   transpileDependencies: true,
-});
+  chainWebpack: (config) => {
+    config.resolve.alias.set("@", path.resolve(__dirname, "src"));
+    // 在這裡添加其他的別名
+    // 例如：.set('@components', path.resolve(__dirname, 'src/components'))
+  },
+};
